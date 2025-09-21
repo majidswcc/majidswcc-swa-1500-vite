fdb7a4f631a957a61ce63cdfdfb75d3ca6cad42e
majidswcc <133676939+majidswcc@users.noreply.github.com>
2025-09-21 22:52:41 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import CompanyOpportunitiesPreview from './CompanyOpportunitiesPreview.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CompanyOpportunitiesPreview />
+  </React.StrictMode>
+)
